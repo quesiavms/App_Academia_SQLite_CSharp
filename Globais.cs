10de7529913676ae43e40ec294a8513ce2d94cc9
Quesia Silva <quesiavirginia2806@gmail.com>
2025-01-28 11:46:39 -0300

    feat: generate application installer

--- Globais.cs
+++ Globais.cs
@@ -11,19 +11,12 @@ namespace App_Academia
         public static string versao = "1.0";
         public static bool logado = false;
         public static int nivel = 0; //0 - basico, 1 - gerente, 2 - master
-        public static string caminho = System.Environment.CurrentDirectory;
+        //public static string caminho = System.Environment.CurrentDirectory; //pegando caminho do programa que esta o projeto
+        public static string caminho = System.AppDomain.CurrentDomain.BaseDirectory.ToString(); //ambiente onde a aplicação esta sendo rodada
         public static string nomeBanco = "db_academia";
         public static string caminhoBanco = caminho +@"\db\";
         public static string caminhoFoto = caminho + @"\fotos\";
         public static string caminhoCarteirinha = caminho + @"\carteirinha\";
 
-        /*
-        N_IDUSUARIO
-        T_NOMEUSUARI0
-        T_USERNAME
-        T_SENHAUSUARIO
-        T_STATUSUSUARIO
-        N_NIVELUSUARIO         
-        */
     }
 }
